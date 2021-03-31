7b116f513657fcc295e413021cff18f52e0b4829
dlpuertaj <dlpuerta@gmail.com>
2021-03-30 22:08:15 -0500

    Solución de errores

--- src/main/java/com/co/dlp/plibrary/entities/Book.java
+++ src/main/java/com/co/dlp/plibrary/entities/Book.java
@@ -8,11 +8,6 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@EqualsAndHashCode
 @Entity
 public class Book  implements Serializable {
 
@@ -30,4 +25,81 @@ public class Book  implements Serializable {
     private Genre genre;
 
     private String imageUrl;
+
+    public Book() {
+    }
+
+    public Book(String title, String isbn, Integer pages, String published, String author, Genre genre, String imageUrl) {
+        this.title = title;
+        this.isbn = isbn;
+        this.pages = pages;
+        this.published = published;
+        this.author = author;
+        this.genre = genre;
+        this.imageUrl = imageUrl;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public void setPages(Integer pages) {
+        this.pages = pages;
+    }
+
+    public String getPublished() {
+        return published;
+    }
+
+    public void setPublished(String published) {
+        this.published = published;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
